bcf7969f25760d6b5fe158d7f4968316322ca5ad
Superpipo <75252692+noafouich@users.noreply.github.com>
2023-12-22 01:40:53 +0100

    Add files via upload
    juste les fonctions de déplacement avec X inclus

--- main.c
+++ main.c
@@ -1,117 +1,186 @@
 #include <stdio.h>
-#include "raylib.h"
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-// CONSTANTS //
+typedef enum {X=-1} VALEUR;
 
-#define WINDOW_W  1280
-#define WINDOW_H  720
+int deplacement_bas(int taille,VALEUR tableau[][taille]){
+    int changement = 0;
+    for (int i=0; i<taille; i++){
+        int curseur = taille-1;
+        int cible = taille-2;
+        while (cible >= 0){
+            if (tableau[curseur][i]==X){
+                curseur--;
+                cible--;
+            }
+            else if (tableau[cible][i]==X){
+                curseur = cible - 1;
+                cible = curseur - 1;
+            }
+            else if (tableau[curseur][i]==0){
+                tableau[curseur][i]=tableau[cible][i];
+                tableau[cible][i]=0;
+                cible--;
+                if (!changement){changement=1;};
+            }
+            else if (tableau[curseur][i]==tableau[cible][i]){
+                tableau[curseur][i] += tableau[cible][i];
+                tableau[cible][i] = 0;
+                cible--;
+                curseur--;
+                if (!changement){changement=1;};
+            }
+            else if (tableau[curseur][i]!=tableau[cible][i]){
+                if ( (curseur-1) != cible){
+                    tableau[curseur-1][i]=tableau[cible][i];
+                    tableau[cible][i]=0;
+                    if (!changement){changement=1;};
+                }
+                curseur--;
+                cible--;
+            }
+        }
+    }
+    return changement;
+}
 
-//----------------------------------------------------------------------------------
-// Types and Structures Definition
-//----------------------------------------------------------------------------------
+int deplacement_droite(int taille,VALEUR tableau[][taille]){
+    int changement = 0;
+    for (int i=0; i<taille; i++){
+        int curseur = taille-1;
+        int cible = taille-2;
+        while (cible >= 0){
+            if (tableau[i][curseur]==X){
+                curseur--;
+                cible--;
+            }
+            else if (tableau[i][cible]==X){
+                curseur = cible - 1;
+                cible = curseur - 1;
+            }
+            else if (tableau[i][curseur]==0){
+                tableau[i][curseur]=tableau[i][cible];
+                tableau[i][cible]=0;
+                cible--;
+                if (!changement){changement=1;};
+            }
+            else if (tableau[i][curseur]==tableau[i][cible]){
+                tableau[i][curseur] += tableau[i][cible];
+                tableau[i][cible] = 0;
+                cible--;
+                curseur--;
+                if (!changement){changement=1;};
+            }
+            else if (tableau[i][curseur]!=tableau[i][cible]){
+                if ( (curseur-1) != cible){
+                    tableau[i][curseur-1]=tableau[i][cible];
+                    tableau[i][cible]=0;
+                    if (!changement){changement=1;};
+                }
+                curseur--;
+                cible--;
+            }
+        }
+    }
+    return changement;
+}
+
+int deplacement_gauche(int taille,VALEUR tableau[][taille]){
+    int changement = 0;
+    for (int i=0; i<taille; i++){
+        int curseur = 0;
+        int cible = 1;
+        while (cible < taille){
+            if (tableau[i][curseur]==X){
+                curseur++;
+                cible++;
+            }
+            else if (tableau[i][cible]==X){
+                curseur = cible + 1;
+                cible = curseur + 1;
+            }
+            else if (tableau[i][curseur]==0){
+                tableau[i][curseur]=tableau[i][cible];
+                tableau[i][cible]=0;
+                cible++;
+                if (!changement){changement=1;};
+            }
+            else if (tableau[i][curseur]==tableau[i][cible]){
+                tableau[i][curseur] += tableau[i][cible];
+                tableau[i][cible] = 0;
+                cible++;
+                curseur++;
+                if (!changement){changement=1;};
+            }
+            else if (tableau[i][curseur]!=tableau[i][cible]){
+                if ( (curseur+1) != cible){
+                    tableau[i][curseur+1]=tableau[i][cible];
+                    tableau[i][cible]=0;
+                    if (!changement){changement=1;};
+                }
+                curseur++;
+                cible++;
+            }
+        }
+    }
+    return changement;
+}
+
+int deplacement_haut(int taille,VALEUR tableau[][taille]){
+    int changement = 0;
+    for (int i=0; i<taille; i++){
+        int curseur = 0;
+        int cible = 1;
+        while (cible < taille){
+            if (tableau[curseur][i]==X){
+                curseur++;
+                cible++;
+            }
+            else if (tableau[cible][curseur]==X){
+                curseur = cible + 1;
+                cible = curseur + 1;
+            }
+            else if (tableau[curseur][i]==0){
+                tableau[curseur][i]=tableau[cible][i];
+                tableau[cible][i]=0;
+                cible++;
+                if (!changement){changement=1;};
+            }
+            else if (tableau[curseur][i]==tableau[cible][i]){
+                tableau[curseur][i] += tableau[cible][i];
+                tableau[cible][i] = 0;
+                cible++;
+                if (!changement){changement=1;};
+            }
+            else if (tableau[curseur][i]!=tableau[cible][i]){
+                if ( (curseur+1) != cible){
+                    tableau[curseur+1][i]=tableau[cible][i];
+                    tableau[cible][i]=0;
+                    if (!changement){changement=1;};
+                }
+                curseur++;
+                cible++;
+            }
+        }
+    }
+    return changement;
+}
 
 
 
-//----------------------------------------------------------------------------------
-// Global Variables Declaration
-//----------------------------------------------------------------------------------
 
-Camera2D camera;
-
-//----------------------------------------------------------------------------------
-// Module Functions Declaration (local)
-//----------------------------------------------------------------------------------
-void DrawSprites();
-void SetFullscreen();
-void GameInput();
-void GameFrame();
-void InitGame();
-
-
-//----------------------------------------------------------------------------------
-// Program main entry point
-//----------------------------------------------------------------------------------
 int main(){
-
-    InitGame();
-
-    while (!WindowShouldClose()){
-
-        SetFullscreen();
-
-        GameInput();
-
-        GameFrame();
-
-        DrawSprites();
-    }
-
-    /* CLEANING AND CLOSING */
-    CloseWindow();
-    return 0;
-}
-
-
-//----------------------------------------------------------------------------------
-// Module Functions Definitions (local)
-//----------------------------------------------------------------------------------
-void SetFullscreen() {
-    /* FULLSCREEN */
-    if(IsKeyPressed(KEY_ENTER) && IsKeyDown(KEY_LEFT_ALT)) {
-        // see what display we are on right now
-        int display = GetCurrentMonitor();
-        
-        if(IsWindowFullscreen()) {
-            // if we are full screen, then go back to the windowed size
-            SetWindowSize(WINDOW_W, WINDOW_H);
+    int taille = 3;
+    VALEUR tableau[][3] = {{X,2,2},
+                           {2,2,X},
+                           {2,2,4}};
+    deplacement_haut(3,tableau);
+    for (int i=0;i<taille;i++){
+        for (int j=0;j<taille; j++){
+            printf("%d",tableau[i][j]);
         }
-        else {
-            // if we are not full screen, set the window size to match the monitor we are on
-            SetWindowSize(GetMonitorWidth(display), GetMonitorHeight(display));
-        }
-        // toggle the state	
-        ToggleFullscreen();
+        printf("\n");
     }
-}
-
-void InitGame(){
-
-    InitWindow(WINDOW_W, WINDOW_H, "test");
-    
-    // basic defaults
-    SetTargetFPS(60);
-    // HideCursor();
-
-    //camera init
-    camera.offset = (Vector2){WINDOW_W/2,WINDOW_H/2};
-    camera.target = (Vector2){WINDOW_W/2, WINDOW_H/2};
-    camera.rotation = 0.0f;
-    camera.zoom = 1.0f;
-}
-
-void GameInput(){
-    if (IsKeyDown(KEY_A)){
-        camera.target.x -= 4;
-    }else if (IsKeyDown(KEY_D)){
-    	camera.target.x += 4;
-    }
-    if (IsKeyDown(KEY_W)){
-        camera.target.y -= 4;
-    }else if (IsKeyDown(KEY_S)){
-    	camera.target.y += 4;
-    }
-
-}
-
-void GameFrame(){
-}
-
-void DrawSprites(){
-    BeginDrawing();
-    ClearBackground(BLACK);
-    BeginMode2D(camera);
-    DrawRectangle(200, 300, 400, 300, WHITE);
-    EndMode2D();
-    DrawRectangle(WINDOW_W/2-20,WINDOW_H/2-20,40,40, RED);
-    EndDrawing();
 }
